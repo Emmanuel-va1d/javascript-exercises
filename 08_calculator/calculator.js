27043e8ea11a30b37d98d445cbfa3e0841e91ddb
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-03-26 01:21:36 -0700

    08_CALCULATOR: created functions to do a range of calculations. Tested the spec file using npm and passed all tests.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let total = 0;
+  for (let i of array)
+    total += i;
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (let i of array)
+    total *= i;
+  return total;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x **= y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let total = 1;
+  for (let i = 1; i <= x; i++)
+    total *= i;
+  return total;
 };
 
 // Do not edit below this line
