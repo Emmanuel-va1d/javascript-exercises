d08ecbd50b52549e830e429970073921b58ac9ff
emmanuel-va1d <vald.emmanuel04@gmail.com>
2023-03-26 00:40:27 -0700

    05_SUMALL: returned the sum of every number in between two number, exclusively. I then made sure to check for errors. Used npm and passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let total = 0;
+    if (x > y) {
+        y = x;
+        x = 1;
+    } else if (x < 0 || y < 0 || typeof x != "number" || typeof y != "number")
+        return "ERROR";
+    for (let i = x; i <= y; i++)
+        total += i;
+    return total;
 };
 
 // Do not edit below this line
